1f133f235726386f5639499c6ebe19f7cdf4513e
Luis Fernando <luisfer.th93@gmail.com>
2023-11-30 17:24:22 -0600

    base de datos sqlite en agencia

---
+++ practica1/src/main/java/lsvp/lfth/p1/agencia/Prueba.java
@@ -0,0 +1,50 @@
+package lsvp.lfth.p1.agencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Prueba {
+
+    public static void main(String[] args){
+        Connection conn= null;
+
+        try{
+            conn = DriverManager.getConnection("jdbc:sqlite:db/test.db");
+            Statement statement = conn.createStatement();
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+          statement.executeUpdate("drop table if exists person");
+          statement.executeUpdate("create table person (id integer, name string)");
+          statement.executeUpdate("insert into person values(1, 'leo')");
+          statement.executeUpdate("insert into person values(2, 'yui')");
+          ResultSet rs = statement.executeQuery("select * from person");
+          while(rs.next())
+          {
+            // read the result set
+            System.out.println("name = " + rs.getString("name"));
+            System.out.println("id = " + rs.getInt("id"));
+          }
+        } catch(SQLException e)
+        {
+          // if the error message is "out of memory",
+          // it probably means no database file is found
+          System.err.println(e.getMessage());
+        }
+        finally
+        {
+          try
+          {
+            if(conn != null)
+              conn.close();
+          }
+          catch(SQLException e)
+          {
+            // connection close failed.
+            System.err.println(e.getMessage());
+          }
+        }
+    }
+}
