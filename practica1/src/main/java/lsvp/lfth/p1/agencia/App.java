016f43c70e19b40d51cfad62ba5ecf39a628c2bc
Luis Torres H <luisfer.th93@gmail.com>
2023-12-03 16:27:40 -0600

    App en agencia

---
+++ practica1/src/main/java/lsvp/lfth/p1/agencia/App.java
@@ -0,0 +1,15 @@
+package lsvp.lfth.p1.agencia;
+
+public class App {
+    private Kia kia;
+    private Nissan nissan;
+    
+    public App(){
+        this.kia = new Kia();
+        this.nissan = new Nissan();
+    }
+
+    public void ejecutar(){
+
+    }
+}
